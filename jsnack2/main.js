8680e49012e641bcda6180d0378faa6460adf2bd
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-01-11 15:45:36 +0100

    conclusione jSnack2

---
+++ jsnack2/main.js
@@ -0,0 +1,18 @@
+const startButton = document.querySelector("h1")
+let numeroUtente
+
+startButton.addEventListener("click", function(){
+
+    {
+        let cubedNum
+        numeroUtente = prompt("Inserisci fino a quale numero vuoi sapere il cubo", "3")
+        numeroUtente = parseInt(numeroUtente)
+        document.querySelector("p").innerHTML = ``
+
+        for (let i=1; i<=numeroUtente; i++){
+            cubedNum= i*i*i
+            document.querySelector("p").innerHTML += `Il cubo del ${i}° numero è: ${cubedNum} <br>`
+        }
+    }
+}
+)
